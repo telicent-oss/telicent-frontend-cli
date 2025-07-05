38e47723a145a57e6390a70711555fabab7637de
A Coolman <153610820+ACoolmanTelicent@users.noreply.github.com>
2025-07-05 16:54:19 +0100

    fix(actions/cache): 3 -> 4 (#48)
    * fix(actions/cache): 3 -> 4
    
    * fix: udpate dist/
    
    ---------
    
    Co-authored-by: ACoolmanTelicent <a.coleman@telicent.io>

--- dist/utils/runScript.js
+++ dist/utils/runScript.js
@@ -1,14 +1,19 @@
 import { spawn } from 'child_process';
 import path from 'path';
 import { dirname } from './dirname.js';
+/**
+ * Runs a shell script as a child processes, ensuring the there is
+ * access to telicent-frontend-cli's  working directory (and environment)
+ * so the shell scripts in this repo can can source other files in this repo
+ *
+ * Higher order function for config of command's action
+ */
 export const runScript = (scriptLoc, args = []) => 
 /**
+ * Lower order function for command's action
  */
 (options, _command) => {
-    console.log('scriptLoc', scriptLoc);
-    console.log('dirname', dirname);
     const scriptPath = path.join(dirname, scriptLoc);
-    console.log('scriptPath', scriptPath);
     const proc = spawn('bash', [scriptPath, ...args], {
         cwd: process.cwd(), // current working directory should be the repo root
         stdio: 'inherit'
