b749f74d0fd964502ed2a7d9721b98d0884e9d52
ACoolmanTelicent <a.coleman@telicent.io>
2025-07-05 16:53:08 +0100

    fix: udpate dist/

--- dist/utils/runScript.d.ts
+++ dist/utils/runScript.d.ts
@@ -1,5 +1,12 @@
 import type { Command } from 'commander';
 interface RunScriptOptions {
 }
+/**
+ * Runs a shell script as a child processes, ensuring the there is
+ * access to telicent-frontend-cli's  working directory (and environment)
+ * so the shell scripts in this repo can can source other files in this repo
+ *
+ * Higher order function for config of command's action
+ */
 export declare const runScript: (scriptLoc: string, args?: string[]) => (options: RunScriptOptions, _command: Command) => void;
 export {};
